ada145c01c8e660bbdb1b696872a8003cfa0c2b3
Enrico B. Palma <enricob.palma@gmail.com>
2025-02-25 15:23:34 -0300

    Pre-adds the Header and Footer components to the App render

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 
+import Header from './components/Header'
+import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
 import ContactPage from './pages/ContactPage'
 import LoginPage from './pages/LoginPage'
@@ -8,28 +10,32 @@ import RegisterPage from './pages/RegisterPage'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app">
-        {/* Navigation */}
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
-          </ul>
-        </nav>
+    <>
+      <Header />
+      <BrowserRouter>
+        <div className="app">
+          {/* Navigation */}
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/register">Register</Link></li>
+            </ul>
+          </nav>
 
-        {/* Route definitions */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="*" element={<div>Page not found</div>} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+          {/* Route definitions */}
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="*" element={<div>Page not found</div>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+      <Footer />
+    </>
   )
 }
 
